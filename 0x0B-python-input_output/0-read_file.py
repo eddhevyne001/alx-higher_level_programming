f45f11684a98b2ff39e0911f0712e1219df7e69c
eddhevyne001 <dhevyne001@gmail.com>
2023-10-10 14:04:49 -0700

    upt

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""read function"""
+
 def read_file(filename=""):
     """ function that reads a text file (UTF8) and prints it to stdout """
 
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""write_file with two arguments"""
 
 
-def number_of_lines(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        return len(list(f))
+def number_of_lines(filename="", text=""):
+    """reads file with utf-8"""
+    with open(filename, "w", encoding='UTF-8') as f:
+        return f.write(text)
