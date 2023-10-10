0752240014a60c432ea1c3044c2343458c58e461
eddhevyne001 <dhevyne001@gmail.com>
2023-10-10 13:56:35 -0700

    upt

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-"""
-0-read_file module
-"""
 
 
-def read_file(filename=""):
-    """
-    read_file - reads a text file (UTF8) and prints it to stdout
-    Args:
-        filename: name of the file
-    """
+def number_of_lines(filename=""):
     with open(filename, "r", encoding="UTF-8") as f:
-        for line in f:
-            print(line, end="")
+        return len(list(f))
